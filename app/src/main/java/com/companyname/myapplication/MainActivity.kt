5f9abe7354428f03fac17366baaadbf75e7f753d
Eun-Taek Jeong <eun1310434@naver.com>
2020-12-04 20:33:32 +0100

    added comment

--- app/src/main/java/com/companyname/myapplication/MainActivity.kt
+++ app/src/main/java/com/companyname/myapplication/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val sad = findViewById<ImageView>(R.id.sad)
         sad.setOnClickListener {
             test.setText("sad")
-            //hallo welt.
+            //hallo welt. 정은택
         }
     }
 
